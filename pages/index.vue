5940a1578fc1a41d5057069c0e727aea5660545b
mizok-o <mizoko320@gmail.com>
2021-01-11 22:51:52 +0900

    環境構築

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div.container
+  logo
+  h1.title tabiShindan
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components:{
+    Logo
+  }
+}
+
+</script>
+
+<style lang="sass">
+
+</style>
