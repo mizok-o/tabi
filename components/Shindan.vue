01b83acca506d0ebace4c7aa0c5ecc1568e16a65
mizok-o <mizoko320@gmail.com>
2021-01-13 16:49:20 +0900

    nameを設定とチェック画像の挿入

--- components/Shindan.vue
+++ components/Shindan.vue
@@ -4,7 +4,7 @@
   p.q__text {{ question.text }}
   .q__content
     ul.q__list
-      QItem(v-for="(item, i) in question.questionList", :key="i", :item="item")
+      QItem(v-for="(item, i) in question.questionList", :key="'q-' + i", :item="item")
   button(@click="shindanStart").index__button 次の問題へ
 </template>
 
@@ -24,7 +24,6 @@ export default {
 </script>
 
 <style lang="sass">
-
 .q__text
   margin-top: 24px
   font-size: 20px
@@ -32,44 +31,5 @@ export default {
 
 .q__content
   margin: 64px 0 0
-
-.q__list__item
-  position: relative
-  display: flex
-  width: 320px
-  height: 56px
-  justify-content: center
-  align-items: center
-  margin-top: 24px
-  border-radius: 28px
-  background-color: #ffffff
-
-.q__list__item-radio
-  position: absolute
-  top: 0
-  left: 0
-  width: 100%
-  height: 100%
-  opacity: 0
-  cursor: pointer
-  &:checked + .q__list__item-text
-    &::before
-      opacity: 1
-
-.q__list__item-text
-  color: #111
-  font-weight: bold
-  &::before
-    content: ""
-    position: absolute
-    top: 50%
-    left: 16px
-    transform: translateY(-50%)
-    display: block
-    width: 24px
-    height: 24px
-    opacity: 0
-    border: 1px solid #111111
-    border-radius: 50%
 
 </style>
