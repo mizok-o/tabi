1511b53f737fc8353e15bc125bc14a8ffee07e32
mizok-o <mizoko320@gmail.com>
2021-01-13 19:06:12 +0900

    診断結果にテキスト、画像、シェアを追加

--- components/Result.vue
+++ components/Result.vue
@@ -1,12 +1,35 @@
 <template lang="pug">
 .result__container
   h1.result__title 診断結果
-  p.result__text あなたのおすすめの国は台湾です。
   .result__content
+    p.result__text
+      |あなたにおすすめの国は
+      br
+      span.result__country 台湾
+      br
+      |です。
+    .result-img
+  .result__share
+    p シェア
   button.index__button もう一度やる
 </template>
 <script>
 </script>
 <style lang="sass">
+.result__text
+  margin-top: 24px
+  line-height: 1.5
 
+.result__country
+  font-size: 40px
+  font-weight: bold
+
+.result-img
+  width: 200px
+  height: 200px
+  margin: 24px auto 0
+  background-color: #333333
+
+.result__share
+  margin-top: 24px
 </style>
