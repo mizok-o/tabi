c5a26be6008bf27e3bf56023d9c36f3ae056e60a
mizok-o <mizoko320@gmail.com>
2021-01-13 13:04:43 +0900

    1問目の静的コーディング

--- components/Shindan.vue
+++ components/Shindan.vue
@@ -1,17 +1,73 @@
 <template lang="pug">
 .q__container
   h1 第一問
-  ul.q__content
-    li a
-    li a
-    li a
+  p あなたの好きな食べ物は？
+  .q__content
+    ul.q__list
+      li.q__list__item
+        input.q__list__item-radio(type="radio")
+        p.q__list__item-text A: 質問です
+      li.q__list__item
+        input.q__list__item-radio(type="radio")
+        p.q__list__item-text B: 質問です
+      li.q__list__item
+        input.q__list__item-radio(type="radio")
+        p.q__list__item-text C: 質問です
+  button(@click="shindanStart").index__button 次の問題へ
 </template>
 
 <script>
-
+export default {
+  methods: {
+    shindanStart() {
+      this.$emit('upNumber', 1);
+    }
+  }
+}
 </script>
 
 <style lang="sass">
 .q__container
   text-align: center
+
+.q__content
+  margin: 64px 0 0
+
+.q__list__item
+  position: relative
+  display: flex
+  width: 320px
+  height: 56px
+  justify-content: center
+  align-items: center
+  margin: 24px 0 0
+  border-radius: 28px
+  background-color: #ffffff
+
+.q__list__item-radio
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  opacity: 0
+  cursor: pointer
+  &:checked + .q__list__item-text
+    &::before
+      opacity: 1
+
+.q__list__item-text
+  color: #111
+  font-weight: bold
+  &::before
+    content: ""
+    position: absolute
+    top: 50%
+    left: 16px
+    transform: translateY(-50%)
+    display: block
+    width: 24px
+    height: 24px
+    border-radius: 50%
+
 </style>
