c86d5f3cf14c4e6dcb4915a19ba9966c736dfb05
mizok-o <mizoko320@gmail.com>
2021-01-14 21:26:23 +0900

    QItemを移行

--- components/Shindan.vue
+++ components/Shindan.vue
@@ -4,26 +4,29 @@
   p.q__text {{ qChild.text }}
   .q__content
     ul.q__list
-      QItem(
+      li.q__list__item(
         v-for="(item, index) in qChild.questionList", :key="'q-' + index"
         :item="item"
-        :qGrand="qChild"
         v-model="answer")
+        input.q__list__item-radio(
+          type="radio"
+          :name="'q-' + item.id"
+          :value="value"
+          @input="$emit('input', item.sArea)")
+        p.q__list__item-text {{ item.text }}
   button(@click="toResult" v-if="buttonN === 5").index__button 診断結果を見る
   button(@click="toNext" v-else).index__button 次の問題へ
 
 </template>
 
 <script>
-import QItem from '~/components/QItem.vue'
-
 export default {
   props: {
     qChild: Object,
-    buttonN: Number
-  },
-  components: {
-    QItem
+    buttonN: Number,
+    value: {
+      type: ''
+    }
   },
   data() {
     return {
@@ -32,7 +35,6 @@ export default {
       as: "",
       us: "",
       A: [],
-
     }
   },
   computed: {
@@ -62,4 +64,43 @@ export default {
 .q__content
   margin: 64px 0 0
 
+.q__list__item
+  position: relative
+  display: flex
+  width: 320px
+  height: 56px
+  justify-content: center
+  align-items: center
+  margin-top: 24px
+  border-radius: 28px
+  background-color: $color-base
+
+.q__list__item-radio
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  opacity: 0
+  cursor: pointer
+  &:checked + .q__list__item-text
+    &::before
+      opacity: 1
+
+.q__list__item-text
+  color: $color-main
+  font-weight: bold
+  &::before
+    content: ""
+    position: absolute
+    top: 50%
+    left: 16px
+    transform: translateY(-50%)
+    display: block
+    width: 24px
+    height: 24px
+    opacity: 0
+    background-size: contain
+    background-repeat: no-repeat
+    background-image: url('~assets/img/check.svg')
 </style>
